63367d1b122e66fe93430ec4b51a8b17c3975f5f
Tamas Szuromi <tamas@codesandbox.io>
2022-06-29 15:14:54 +0000

    Add static sitemap generation

---
+++ packages/sandbox-docs/next-sitemap.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: "https://codesandbox.io/",
+  generateRobotsTxt: false,
+  sitemapSize: 7000,
+  changefreq: "weekly",
+
+  transform: async (config, path) => {
+    return {
+      loc: path.replace(".en-US", ""),
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
+};
+
+module.exports = config;
