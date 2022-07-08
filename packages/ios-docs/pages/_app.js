668bad88d50af94448945b4b589bc6f3c1ddaf89
Tamas Szuromi <tamas@codesandbox.io>
2022-07-08 22:37:44 +0000

    Add amplitude to all sites

--- packages/ios-docs/pages/_app.js
+++ packages/ios-docs/pages/_app.js
@@ -1,7 +1,52 @@
 import "codesandbox-theme-docs/style.css";
 import "../styles.css";
+import amplitude from "amplitude-js";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Nextra({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
+  const router = useRouter();
+
+  // Init Amplitude
+  amplitude.getInstance().init("a205ed9b06a7baf5a594bdd30293aa80", null, {
+    includeReferrer: true,
+    saveEvents: true,
+    includeUtm: true,
+    saveParamsReferrerOncePerSession: false,
+  });
+
+  const isBrowser = typeof window !== "undefined";
+  const [initialRouteTracked, setInitialRouteTracked] = useState(false);
+
+  useEffect(() => {
+    if (isBrowser && !initialRouteTracked && window.location.search === "") {
+      const event = "pageview";
+      const eventProperties = {
+        source: "docs",
+        path: router.asPath,
+      };
+      amplitude.getInstance().logEvent(event, eventProperties);
+      setInitialRouteTracked(true);
+    }
+  }, [router]);
+
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = (path) => {
+      const event = "pageview";
+      const eventProperties = {
+        source: "docs",
+        path: path,
+      };
+      amplitude.getInstance().logEvent(event, eventProperties);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
   return getLayout(<Component {...pageProps} />);
 }
