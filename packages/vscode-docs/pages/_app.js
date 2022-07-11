37ae5bd2a59a79c9dbfb6d7212f19e5b7af1536c
Tamas Szuromi <tromika@gmail.com>
2022-07-11 06:54:34 +0200

    Add Amplitude tracking (#26)
    * Add Amplitude
    
    * additional amplitude params
    
    * Add amplitude to all sites
    
    * Add amplitude env var; remove unnecessary first load condition
    
    Co-authored-by: Tamas Szuromi <tamas@codesandbox.io>

--- packages/vscode-docs/pages/_app.js
+++ packages/vscode-docs/pages/_app.js
@@ -1,7 +1,52 @@
 import "codesandbox-theme-docs/style.css";
 import "../styles.css";
+import amplitude from "amplitude-js";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Nextra({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
+  const router = useRouter();
+
+  // Init Amplitude
+  amplitude.getInstance().init(process.env.NEXT_PUBLIC_AMPLITUDE, null, {
+    includeReferrer: true,
+    saveEvents: true,
+    includeUtm: true,
+    saveParamsReferrerOncePerSession: false,
+  });
+
+  const isBrowser = typeof window !== "undefined";
+  const [initialRouteTracked, setInitialRouteTracked] = useState(false);
+
+  useEffect(() => {
+    if (isBrowser && !initialRouteTracked) {
+      const event = "pageview";
+      const eventProperties = {
+        source: "docs",
+        path: router.asPath,
+      };
+      amplitude.getInstance().logEvent(event, eventProperties);
+      setInitialRouteTracked(true);
+    }
+  }, [router]);
+
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = (path) => {
+      const event = "pageview";
+      const eventProperties = {
+        source: "docs",
+        path: path,
+      };
+      amplitude.getInstance().logEvent(event, eventProperties);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
   return getLayout(<Component {...pageProps} />);
 }
