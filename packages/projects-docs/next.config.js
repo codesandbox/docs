aa6721f437155a1121975a0c42e24273fa1c70c8
Zeh Fernandes <ozehfernandes@gmail.com>
2022-07-12 11:24:41 -0300

    Restructure URL system

--- packages/projects-docs/next.config.js
+++ packages/projects-docs/next.config.js
@@ -1,3 +1,5 @@
+const BASE_PATH = process.env.BASE_PATH;
+
 const withNextra = require("nextra")({
   theme: "codesandbox-theme-docs",
   themeConfig: "./theme.config.js",
@@ -12,9 +14,15 @@ const withTM = require("next-transpile-modules")(["../../shared-components"]);
 
 module.exports = withTM(
   withNextra({
-    i18n: {
-      locales: ["en-US"],
-      defaultLocale: "en-US",
+    basePath: BASE_PATH || "",
+    async redirects() {
+      return [
+        {
+          source: "/",
+          destination: "/introduction/overview",
+          permanent: true,
+        },
+      ];
     },
   })
 );
