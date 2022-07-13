4eb601700356b1326358be0a172c93c550ee720b
Tamas Szuromi <tamas@codesandbox.io>
2022-07-13 15:14:53 +0000

    Fix sitemap urls

--- packages/projects-docs/next-sitemap.config.js
+++ packages/projects-docs/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: "https://codesandbox.io/projects/",
+  siteUrl: "https://codesandbox.io/docs/projects/",
   generateRobotsTxt: false,
   sitemapSize: 7000,
   changefreq: "weekly",
