ba91c8d7ceaf5a5ba9c7cc714c6f0d4406520d5f
Zeh Fernandes <ozehfernandes@gmail.com>
2022-10-06 16:01:00 -0300

    redirects

--- packages/projects-docs/next.config.js
+++ packages/projects-docs/next.config.js
@@ -1,4 +1,5 @@
 const BASE_PATH = process.env.BASE_PATH;
+const OLD_BASE_PATH = "";
 
 const withNextra = require("nextra")({
   theme: "codesandbox-theme-docs",
@@ -17,6 +18,9 @@ module.exports = withTM(
     basePath: BASE_PATH || "",
     async redirects() {
       return [
+        /*
+          Redirects to avoid inital page
+        */
         {
           source: "/",
           destination: "/learn/introduction/overview",
@@ -25,6 +29,283 @@ module.exports = withTM(
         {
           source: "/tutorial/docker",
           destination: "/tutorial/getting-started-with-docker",
+          permanent: true,
+        },
+        /*
+           Redirects to support old website structure
+           Migration between
+
+           codesandbox.io/docs/projects
+           codesandbox.io/docs/ios
+           codesandbox.io/docs/vscode     =>  codesandbox.io/docs
+           codesandbox.io/docs/
+        */
+
+        // Sandbox
+        // ---------------------------
+        {
+          source: `${OLD_BASE_PATH}/overview`,
+          destination: "/learn/introduction/overview",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/workspaces`,
+          destination: "/learn/introduction/workspace",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/embedding`,
+          destination: "/learn/sandboxes/embedding",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/git`,
+          destination: "/learn/repositories/overview",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/uploading`,
+          destination: "/learn/sandboxes/editors",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/importing`,
+          destination: "/learn/getting-started/your-first-sandbox",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/importing`,
+          destination: "/learn/getting-started/collaborate-share",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/tests`,
+          destination: "/learn/sandboxes/tests",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/secrets`,
+          destination: "/learn/sandboxes/secrets",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/deployment`,
+          destination: "/learn/sandboxes/deployment",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/configuration`,
+          destination: "/learn/sandboxes/configuration",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/preferences`,
+          destination: "/learn/teams/preferences",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/templates`,
+          destination: "/learn/sandboxes/templates",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/environment`,
+          destination: "/learn/repositories/env",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/ci`,
+          destination: "/learn/sandboxes/ci",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/custom-npm-registry`,
+          destination: "/learn/sandboxes/npm",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/custom-npm-registry`,
+          destination: "/learn/sandboxes/custom-npm-registry",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/explore`,
+          destination: "/learn/integrations/explore",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/api-reference`,
+          destination: "/learn/getting-started/your-first-sandbox",
+          permanent: true,
+        },
+
+        // VS Code
+        // ---------------------------
+        {
+          source: `${OLD_BASE_PATH}/vscode/overview`,
+          destination: "/learn/introduction/overview",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/vscode/devtools`,
+          destination: "/learn/repositories/devtools",
+          permanent: true,
+        },
+        // TODO: Redirect to open the right tab
+        {
+          source: `${OLD_BASE_PATH}/vscode/open-branches`,
+          destination: "/learn/repositories/editors",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/vscode/live-collaboration`,
+          destination: "/learn/repositories/editors",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/vscode/review-prs`,
+          destination: "/learn/repositories/editors",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/vscode/extensions`,
+          destination: "/learn/repositories/editors",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/vscode/insiders`,
+          destination: "/learn/repositories/editors",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/vscode/faq`,
+          destination: "/faq",
+          permanent: true,
+        },
+
+        // iOS
+        {
+          source: `${OLD_BASE_PATH}/ios/overview`,
+          destination: "/faq",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/ios/faq`,
+          destination: "/learn/introduction/overview",
+          permanent: true,
+        },
+        // TODO: Redirect to open the right tab
+        {
+          source: `${OLD_BASE_PATH}/ios/sandbox`,
+          destination: "/learn/getting-started/your-first-sandbox",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/ios/projects`,
+          destination: "/learn/getting-started/your-first-repository",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/ios/shortcuts`,
+          destination: "/learn/repositories/shortcuts",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/ios/faq`,
+          destination: "/faq",
+          permanent: true,
+        },
+
+        // Projects
+        // ---------------------------
+        {
+          source: `${OLD_BASE_PATH}/projects/:path*'`,
+          destination: "/:path*'",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/projects/learn/introduction/about-projects`,
+          destination: "/learn/editor/overview",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/projects/learn/getting-started/your-first-project`,
+          destination: "/learn/getting-started/your-first-repository",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/projects/learn/getting-started/start-building`,
+          destination: "/learn/getting-started/your-first-repository",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/projects/learn/getting-started/keep-working-on-vscode`,
+          destination: "/learn/repositories/editors",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/projects/learn/getting-started/keep-working-on-ios`,
+          destination: "/learn/repositories/editors",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/projects/learn/editor`,
+          destination: "/learn/repositories/editors",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/projects/learn/editor/devtools`,
+          destination: "/learn/repositories/devtools",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/projects/learn/editor/preview`,
+          destination: "/learn/repositories/preview",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/projects/learn/editor/task`,
+          destination: "/learn/repositories/task",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/projects/learn/editor/terminal`,
+          destination: "/learn/repositories/terminal",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/projects/learn/editor/interactive-readme`,
+          destination: "/learn/repositories/interactive-readme",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/projects/learn/editor/commandpalette`,
+          destination: "/learn/repositories/commandpalette",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/projects/learn/editor/shortcuts`,
+          destination: "/learn/repositories/shortcuts",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/projects/learn/setting-up/tasks`,
+          destination: "/learn/repositories/tasks",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/projects/learn/setting-up/env`,
+          destination: "/learn/repositories/env",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/projects/learn/setting-up/secrets`,
+          destination: "/learn/repositories/secrets",
+          permanent: true,
+        },
+        {
+          source: `${OLD_BASE_PATH}/projects/learn/setting-up/limitations`,
+          destination: "/learn/repositories/limitations",
           permanent: true,
         },
       ];
