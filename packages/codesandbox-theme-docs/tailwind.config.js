4b772625e2fdd477f5bb6f3ab31cee4a938a5c27
Zeh Fernandes <ozehfernandes@gmail.com>
2022-11-08 17:57:51 -0300

    .

--- packages/codesandbox-theme-docs/tailwind.config.js
+++ packages/codesandbox-theme-docs/tailwind.config.js
@@ -1,49 +1,49 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 const makePrimaryColor =
-  l =>
+  (l) =>
   ({ opacityValue }) => {
     if (opacityValue === undefined) {
-      return `hsl(var(--nextra-primary-hue) 100% ${l}%)`
+      return `hsl(var(--nextra-primary-hue) 100% ${l}%)`;
     }
-    return `hsl(var(--nextra-primary-hue) 100% ${l}% / ${opacityValue})`
-  }
+    return `hsl(var(--nextra-primary-hue) 100% ${l}% / ${opacityValue})`;
+  };
 
 module.exports = {
-  prefix: 'nx-',
+  prefix: "nx-",
   content: [
-    './src/**/*.tsx',
-    '../nextra/src/icons/*.tsx',
-    '../nextra/src/components/*.tsx'
+    "./src/**/*.tsx",
+    "../nextra/src/icons/*.tsx",
+    "../nextra/src/components/*.tsx",
   ],
   theme: {
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-      '2xl': '1536px'
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
     fontSize: {
-      xs: '.75rem',
-      sm: '.875rem',
-      base: '1rem',
-      lg: '1.125rem',
-      xl: '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.875rem',
-      '4xl': '2.25rem',
-      '5xl': '3rem',
-      '6xl': '4rem'
+      xs: ".75rem",
+      sm: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
     },
     letterSpacing: {
-      tight: '-0.015em'
+      tight: "-0.015em",
     },
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: '#000',
-      white: '#fff',
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000",
+      white: "#fff",
       gray: colors.gray,
       slate: colors.slate,
       neutral: colors.neutral,
@@ -52,25 +52,25 @@ module.exports = {
       blue: colors.blue,
       yellow: colors.yellow,
       primary: {
-        50: makePrimaryColor(97),
-        100: makePrimaryColor(94),
-        200: makePrimaryColor(86),
-        300: makePrimaryColor(77),
-        400: makePrimaryColor(66),
-        500: makePrimaryColor(50),
+        50: makePrimaryColor(75),
+        100: makePrimaryColor(75),
+        200: makePrimaryColor(75),
+        300: makePrimaryColor(75),
+        400: makePrimaryColor(75),
+        500: makePrimaryColor(75),
         600: makePrimaryColor(45),
-        700: makePrimaryColor(39),
-        750: makePrimaryColor(35),
-        800: makePrimaryColor(32),
-        900: makePrimaryColor(24),
-        1000: makePrimaryColor(12)
-      }
+        700: makePrimaryColor(75),
+        750: makePrimaryColor(75),
+        800: makePrimaryColor(75),
+        900: makePrimaryColor(75),
+        1000: makePrimaryColor(10),
+      },
     },
     extend: {
       colors: {
-        dark: '#111'
-      }
-    }
+        dark: "#111",
+      },
+    },
   },
-  darkMode: ['class', 'html[class~="dark"]']
-}
+  darkMode: ["class", 'html[class~="dark"]'],
+};
