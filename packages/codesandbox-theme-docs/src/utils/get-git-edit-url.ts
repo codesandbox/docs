ab01e68ed3c76099f95cbbb2721b41bb82671e4d
Maria Clara <claarasantana@gmail.com>
2022-12-01 20:19:57 +0000

    fix: remove leftover console

--- packages/codesandbox-theme-docs/src/utils/get-git-edit-url.ts
+++ packages/codesandbox-theme-docs/src/utils/get-git-edit-url.ts
@@ -1,17 +1,16 @@
-import { useConfig } from '../contexts'
-import gitUrlParse from 'git-url-parse'
+import { useConfig } from "../contexts";
+import gitUrlParse from "git-url-parse";
 
 export const getGitEditUrl = (filePath?: string): string => {
-  const config = useConfig()
-  const repo = gitUrlParse(config.docsRepositoryBase || '')
+  const config = useConfig();
+  const repo = gitUrlParse(config.docsRepositoryBase || "");
 
-  if (!repo) throw new Error('Invalid `docsRepositoryBase` URL!')
-  console.log(repo.href);
+  if (!repo) throw new Error("Invalid `docsRepositoryBase` URL!");
 
   const url = new URL(repo.href);
   if (filePath) {
-    url.searchParams.append('file', filePath);
+    url.searchParams.append("file", filePath);
   }
 
   return url.toString();
-}
+};
